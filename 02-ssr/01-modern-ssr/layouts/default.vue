8d0b71b5a88dce80b3e4b356a7ebed6830e5d97b
“dongyuekai” <457545648@qq.com>
2021-03-17 17:48:52 +0800

    同构渲染的demo

---
+++ 02-ssr/01-modern-ssr/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- 路由出口 -->
+    <ul>
+      <li>
+        <!-- 类似于router-link 用于单页面应用导航 -->
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+      <!-- 子页面出口 -->
+      <nuxt />
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
