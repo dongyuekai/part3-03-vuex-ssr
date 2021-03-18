fca904d5404e57d8f80279047824b114bac7eefb
“dongyuekai” <457545648@qq.com>
2021-03-18 11:14:59 +0800

    布局组件layout

---
+++ 03-nuxt-basics/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>layout/default.vue组价</h1>
+    <!-- 页面出口 类似于子路由出口 router-view -->
+    <nuxt />
+  </div>
+</template>
+<script>
+export default {
+  name: "LayoutDefault",
+};
+</script>
---
+++ 03-nuxt-basics/layouts/foo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>layout/foo.vue组价</h1>
+    <!-- 页面出口 类似于子路由出口 router-view -->
+    <nuxt />
+  </div>
+</template>
+<script>
+export default {
+  name: "LayoutFoo",
+};
+</script>
